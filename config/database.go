9cd4d6eaf2a8c67b9a154245dbfbff6e4ff5be39
ajiputraprayogi <edp@MacBook-Pro-EDP.local>
2026-01-30 19:28:01 +0700

    update config new

--- config/database.go
+++ config/database.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,10 +11,24 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "root:@tcp(127.0.0.1:3306)/go_project?charset=utf8mb4&parseTime=True&loc=Local"
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser,
+		dbPass,
+		dbHost,
+		dbPort,
+		dbName,
+	)
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic(err)
 	}
 
 	DB = db
--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"go-project/config"
 	"go-project/models"
 	"go-project/routes"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load .env hanya untuk local
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using system env")
+	}
+
 	config.ConnectDB()	
 	config.DB.AutoMigrate(&models.User{}, &models.Post{})
 
